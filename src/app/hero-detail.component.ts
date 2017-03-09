5511c59b1d128506b751531f031005274c5397e6
Jaime <jaimereycasado@gmail.com>
2017-03-09 03:04:52 +0100

    first tutorial completed

---
+++ src/app/hero-detail.component.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by Jaime on 09/03/2017.
+ */
+
+import { Component, Input } from '@angular/core';
+
+@Component ({
+  selector: 'my-hero-detail'
+})
+
+export  class HeroDetailComponent {
+
+}
