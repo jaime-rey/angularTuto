03a7e1d075a2f09938e5a7ec272e6242aaad19f8
Jaime <jaimereycasado@gmail.com>
2017-03-09 03:48:56 +0100

    finished 4. Multiple Components

---
+++ src/app/hero.js
@@ -0,0 +1,11 @@
+"use strict";
+/**
+ * Created by Jaime on 09/03/2017.
+ */
+var Hero = (function () {
+    function Hero() {
+    }
+    return Hero;
+}());
+exports.Hero = Hero;
+//# sourceMappingURL=hero.js.map
---
+++ src/app/hero.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by Jaime on 09/03/2017.
+ */
+export class Hero {
+  id: number;
+  name: string;
+}
